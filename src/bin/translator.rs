214ac8e46659ca23f40631f003208f49f3430d41
arseeenyyy <rubtsov113@mail.ru>
2025-08-26 15:04:08 +0300

    done with reading/writing into file

--- src/bin/translator.rs
+++ src/bin/translator.rs
@@ -1,5 +1,7 @@
 use csa_stack_machine::isa::*;
+
 use std::fs;
+use std::env;
 use std::io;
 
 pub fn read_programm(file_name: &str) -> Result<String, io::Error> {
@@ -8,13 +10,33 @@ pub fn read_programm(file_name: &str) -> Result<String, io::Error> {
 
 
 fn main() {
-    let file_name = "examples/hello.fth";
-    let content = match read_programm(&file_name) {
-        Ok(content) => content, 
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: translator <source_file> <out_file>");
+        std::process::exit(1);
+    }
+    let source_file = &args[1];
+    let out_file = &args[2];
+
+    println!("{source_file}, {out_file}");
+
+    let program = match read_programm(&source_file) {
+        Ok(text) => text, 
         Err(e) => {
-            eprintln!("Error reading file '{}' : '{}'", file_name, e);
-            return; 
+            eprintln!("Error reading file {} : {}", source_file, e);
+            std::process::exit(1);
         }
     };
-    println!("{content}");
+
+    match fs::write(out_file, program.as_bytes()) {
+        Ok(_) => {
+            println!("Successfully wrote into {}", out_file);
+        }
+        Err(e) => {
+            eprintln!("Error writing into file {} : {}", out_file, e); 
+            std::process::exit(1);
+        }
+    }
+
 }
